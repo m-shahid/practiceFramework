263528e956087e5b3ac70e508f3756afa32e4651
Shahid Sayyad <ssayyad@internal.tavisca.com>
2017-04-20 11:09:58 +0530

    First Commit. Basic framework structure with testNG and Maven pom file

---
+++ src/test/java/tests/Demo.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	
+	@Test
+	public void test(){
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\shahid\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+	}
+}
